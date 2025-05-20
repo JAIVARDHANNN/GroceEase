682a2440be010d8044a85c8f1d9f4afbbb5726c2
JAIVARDHAN RAJPUROHIT <123367563+JAIVARDHANNN@users.noreply.github.com>
2025-05-20 23:47:11 +0530

    Specific Product category created

--- client/src/Pages/ProductCategory.jsx
+++ client/src/Pages/ProductCategory.jsx
@@ -20,8 +20,8 @@ const ProductCategory = () => {
                <div className='w-16 h-0.5 bg-primary rounded-full'></div>  
             </div>
         )}
-        {filteredProducts.length > 0 ? (
-          <div>
+        {filteredProducts.length > 0 ? ( 
+          <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
             {filteredProducts.map((product)=>(
               <ProductCard key={product._id} product= {product} />
             ))}
