ee17f183079a485fa3e15f2158d7dae8004c914a
JAIVARDHAN RAJPUROHIT <123367563+JAIVARDHANNN@users.noreply.github.com>
2025-06-03 01:32:26 +0530

    Connected with Database

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors';
+import connectDB from './configs/db.js';
+
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+await connectDB();
 
 //Allow multiple origins
 const allowedOrigins = ['http://localhost:5173'] //this is basically a list of allowed domains and links to acccess the bacnend localhost:400 for fetching data
 
 //Middleware Configuration
 app.use(express.json());
-app.use(cookieParser);
-app.use(cors({origin: allowedOrigins , Credentials : true}));
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins , credentials : true}));
 
 
 
